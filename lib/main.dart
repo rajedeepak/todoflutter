989309a53cdff82147477ec41fa4764bc68d4d02
Deepak Rajendran <deepakrajendran729@gmail.com>
2020-01-21 20:53:07 +0530

    initial commit

---
+++ lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+import 'screens/HomePage/home-page.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(primarySwatch: Colors.blue,),
+      home: HomePage(),
+    );
+  }
+}
